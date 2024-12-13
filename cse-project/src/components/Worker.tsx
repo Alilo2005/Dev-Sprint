59377d705ddafc7a897521e42b0c89836a57e79e
MAHDI ALI <126531731+Alilo2005@users.noreply.github.com>
2024-12-13 14:38:20 +0000

    Add counter functionality to Worker component and remove StrictMode from main render

--- cse-project/src/components/Worker.tsx
+++ cse-project/src/components/Worker.tsx
@@ -1,6 +1,20 @@
+import { useEffect, useState } from "react"
+
 const Worker = ({name, job, isWorking}: {name: string, job: string, isWorking: boolean}) => {
-  return (
+
+    const [count, setCount] = useState(0)
+    const increment = () => setCount(count + 1)
+    const decrement = () => setCount(count - 1)
+
+    useEffect(() => {
+        console.log(`Worker ${name} is ${isWorking ? 'working' : 'not working'}`)
+    }, [count])
+
+    return (
     <div>
+        <button onClick={increment}>+</button>
+        <button onClick={decrement}>-</button>
+      <h2> Counter is : {count}</h2>
       <h1>{name}</h1>
       <h2>{job}</h2>
       {isWorking && <h3>Working</h3>}
