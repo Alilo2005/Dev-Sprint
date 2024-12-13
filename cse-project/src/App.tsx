5d101ae78333d5c2cd334890596cec3c6d248716
MAHDI ALI <126531731+Alilo2005@users.noreply.github.com>
2024-12-13 14:20:09 +0000

    Replace Name component with Worker component to display worker details

--- cse-project/src/App.tsx
+++ cse-project/src/App.tsx
@@ -1,15 +1,15 @@
 
 import './App.css'
 import './components/Name.tsx'
-import Name from './components/Name.tsx'
+import Worker from './components/Worker'
 function App() {
 
   return (
     <>
-      <Name name="Ali" />
-      <Name name="Ahmed" />
-      <Name name="Umar" />
-      <Name name="Usman" />
+      <Worker name="John" job="Software Engineer" isWorking={true} />
+      <Worker name="Jane" job="Doctor" isWorking={false} />
+      <Worker name="Jack" job="Teacher" isWorking={true} />
+      <Worker name="Jill" job="Nurse" isWorking={false} />
     </>
 
   )
---
+++ cse-project/src/components/Worker.tsx
@@ -0,0 +1,11 @@
+export default function Worker({name, job, isWorking}: {name: string, job: string, isWorking: boolean}) {
+  return (
+    <div>
+      <h1>{name}</h1>
+      <h2>{job}</h2>
+      {isWorking && <h3>Working</h3>}
+      {!isWorking && <h3>Not Working</h3>}
+      </div>
+  )
+
+}
