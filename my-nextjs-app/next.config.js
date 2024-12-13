dea015bdbe373edb04038f2b786469210b1123b0
MAHDI ALI <126531731+Alilo2005@users.noreply.github.com>
2024-12-13 14:56:48 +0000

    Migrate from my-nextjs-app to next-cse, adding initial configuration and assets

--- my-nextjs-app/next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['example.com'], // Replace with your image domains
-  },
-  env: {
-    CUSTOM_API_URL: process.env.CUSTOM_API_URL, // Example of adding environment variables
-  },
-};
